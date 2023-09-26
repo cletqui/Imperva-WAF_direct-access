c60e522b5a0dcf190084c362362f85d0ea8cd4dc
cletqui <antoine.quillivic@gmail.com>
2023-09-26 18:20:20 +0200

    Update check_direct_access.sh

--- check_direct_access.sh
+++ check_direct_access.sh
@@ -1,17 +1,113 @@
 #!/bin/bash
 
-# Retrieve global environment variables stored in .env
-source .env
+print_help() {
+  echo "Usage: ./check_direct_access.sh [OPTIONS]"
+  echo "Options:"
+  echo "  -v, --verbose           Enable verbose mode"
+  echo "  -o, --output FILE.txt   Specify the output file with a .txt extension"
+  echo "  -t, --timeout SECONDS   Specify the timeout in seconds (positive integer)"
+  echo "  --websites-only         Enable websites-only mode"
+  echo "  --env FILE              Specify the path to a .env file for environment variables"
+  echo "  -h, --help              Display this help message"
+  exit 0
+}
 
-# Declare global variables for this script
-TIMEOUT_DURATION="10" # Time before a request is considered as a timeout (in seconds)
+# Declare local variables for this script
+verbose=false # Verbose option
+output_file="" # Output file option (initialized with Imperva account ID by default after the parsing of the options)
+websites_only=false # Extract only the websites option
+timeout_value="10" # Time before a request is considered as a timeout (in seconds, by default 10 seconds)
+env_file=".env" # Environment file path option
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -v|--verbose)
+      verbose=true
+      shift
+      ;;
+    -o|--output)
+      if [[ -n "$2" ]]; then
+        # Check if the output file has a .txt extension
+        if [[ "$2" == *.txt ]]; then
+          output_file="$2"
+          shift 2
+        else
+          echo "Error: Output file must have a .txt extension" >&2
+          exit 1
+        fi
+      else
+        echo "Error: Missing argument for $1" >&2
+        exit 1
+      fi
+      ;;
+    -t|--timeout)
+      if [[ -n "$2" && "$2" =~ ^[0-9]+$ ]]; then
+        timeout_value="$2"
+        shift 2
+      else
+        echo "Error: Timeout must be a positive integer" >&2
+        exit 1
+      fi
+      ;;
+    --websites-only)
+      websites_only=true
+      shift
+      ;;
+    --env)
+      if [[ -n "$2" ]]; then
+        if [[ -f "$2" ]]; then
+          env_file="$2"
+          shift 2
+        else
+          echo "Error: .env file not found at the specified path: $2" >&2
+          exit 1
+        fi
+      else
+        echo "Error: Missing argument for $1" >&2
+        exit 1
+      fi
+      ;;
+    -h|--help)
+      print_help
+      ;;
+    *)
+      echo "Error: Unknown option $1" >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Retrieve global environment variables stored in env_file
+source $env_file
+
+# Check if the output file is still empty, in that case use ACCOUNT_ID from env file to name the output file
+if [[ -z "$output_file" ]]; then
+  if [[ -n "$env_file" && -f "$env_file" ]]; then
+    if [[ -n "$ACCOUNT_ID" ]]; then
+      output_file="${ACCOUNT_ID}.txt"
+    else
+      echo "Error: ACCOUNT_ID is not defined in the .env file" >&2
+      exit 1
+    fi
+  else
+    echo "Error: Output file not specified, and .env file is missing or not specified" >&2
+    exit 1
+  fi
+fi
+
+# Now you can use the parsed options
+echo "Verbose: $verbose"
+echo "Output File: $output_file"
+echo "Websites Only: $websites_only"
+echo "Timeout: $timeout_value seconds"
+echo "Env File: $env_file"
 
 # Print ASCII art
 echo -e "\e[0;34m   ,---,          ____                                                                     .---.   ,---,           ,---,.\n,\`--.' |        ,'  , \`.,-.----.                                                          /. ./|  '  .' \        ,'  .' |\n|   :  :     ,-+-,.' _ |\    /  \             __  ,-.                                 .--'.  ' ; /  ;    '.    ,---.'   |\n:   |  '  ,-+-. ;   , |||   :    |          ,' ,'/ /|    .---.                       /__./ \ : |:  :       \   |   |   .'\n|   :  | ,--.'|'   |  |||   | .\ :   ,---.  '  | |' |  /.  ./|   ,--.--.         .--'.  '   \' .:  |   /\   \  :   :  :  \n'   '  ;|   |  ,', |  |,.   : |: |  /     \ |  |   ,'.-' . ' |  /       \       /___/ \ |    ' '|  :  ' ;.   : :   |  |-,\n|   |  ||   | /  | |--' |   |  \ : /    /  |'  :  / /___/ \: | .--.  .-. |      ;   \  \;      :|  |  ;/  \   \|   :  ;/|\n'   :  ;|   : |  | ,    |   : .  |.    ' / ||  | '  .   \  ' .  \__\/: . .       \   ;  \`      |'  :  | \  \ ,'|   |   .'\n|   |  '|   : |  |/     :     |\`-''   ;   /|;  : |   \   \   '  ,\" .--.; |        .   \    .\  ;|  |  '  '--'  '   :  '  \n'   :  ||   | |\`-'      :   : :   '   |  / ||  , ;    \   \    /  /  ,.  |         \   \   ' \ ||  :  :        |   |  |  \n;   |.' |   ;/          |   | :   |   :    | ---'      \   \ |;  :   .'   \\         :   '  |--\" |  | ,'        |   :  \  \n'---'   '---'           \`---'.|    \   \  /             '---\" |  ,     .-./          \   \ ;    \`--''          |   | ,'  \n                          \`---\`     \`----'                     \`--\`---'               '---\"                    \`----'    \e[0m\n"
 
 # List websites from Imperva account
 echo -e "Retrieving websites from Imperva account (\e[1;30m$ACCOUNT_ID\e[0m)...\n"
-
+exit
 # Make an Imperva API request to list account websites
 response=$(curl -s -X POST -H "accept: application/json" "$API_ENDPOINT/sites/list?api_id=$API_ID&api_key=$API_KEY&account_id=$ACCOUNT_ID")
 
@@ -61,7 +157,7 @@ echo "$response" | jq -c '.sites[]' | while IFS=$'\t' read -r site; do
 
   # Check direct accessibility of origin server IPs
   for ip in $ips; do
-    result=$(timeout "${TIMEOUT_DURATION}" curl -sILk "https://${domain}" --resolve "${domain}:443:${ip}" -o /dev/null -w "%{http_code}")
+    result=$(timeout "${timeout_duration}" curl -sILk "https://${domain}" --resolve "${domain}:443:${ip}" -o /dev/null -w "%{http_code}")
 
     if [[ "${result}" == "200" ]]; then
       echo -e "        \e[31m❗\e[0m Origin IP \e[2m${ip}\e[0m is directly accessible (\e[2mHTTP_CODE $result\e[0m)."
@@ -104,7 +200,7 @@ echo "$response" | jq -c '.sites[]' | while IFS=$'\t' read -r site; do
 
   # Check accessibility of WAF IPs
   for waf_ip in $waf_ips; do
-    result=$(timeout "${TIMEOUT_DURATION}" curl -sILk  "https://${domain}" --resolve "${domain}:443:${waf_ip}" -o /dev/null -w "%{http_code}")
+    result=$(timeout "${timeout_duration}" curl -sILk  "https://${domain}" --resolve "${domain}:443:${waf_ip}" -o /dev/null -w "%{http_code}")
 
     if [[ "${result}" == "200" ]]; then
       echo -e "        \e[32m✅\e[0m WAF IP \e[2m${waf_ip}\e[0m is accessible (\e[2mHTTP_CODE $result\e[0m)."
